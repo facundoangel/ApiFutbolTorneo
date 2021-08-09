e305e6ed5e8fb01f8290522a7287ed3642a09b3c
facundoN1997 <facundonicolasangel@gmail.com>
2021-08-09 00:51:16 -0300

    Api Terminada

---
+++ Migrations/20210809035009_initial.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ApiFutbolTorneo.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FutbolDBEquipos",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    equipo = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    color = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FutbolDBEquipos", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FutbolDBJugadoras",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    dni = table.Column<int>(type: "int", nullable: false),
+                    nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    apellido = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    equipojugando = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FutbolDBJugadoras", x => x.id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "FutbolDBEquipos",
+                columns: new[] { "id", "color", "equipo" },
+                values: new object[,]
+                {
+                    { 1, "celeste", "Arsenal" },
+                    { 2, "rojo", "Independiente" },
+                    { 3, "verde y rojo", "Banfield" },
+                    { 4, "rojo y azul", "Tigre" },
+                    { 5, "celeste y blanco", "Racing" },
+                    { 6, "rojo y blanco", "River" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "FutbolDBJugadoras",
+                columns: new[] { "id", "apellido", "dni", "equipojugando", "nombre" },
+                values: new object[,]
+                {
+                    { 28, "Jacinto", 42840927, "Banfield", "Gabriela" },
+                    { 29, "Suarez", 42940928, "Banfield", "Tamara" },
+                    { 30, "Martinez", 43040929, "Banfield", "Belen" },
+                    { 31, "Espinoza", 43140900, "Banfield", "Violeta" },
+                    { 32, "Godoy", 43240921, "Racing", "Catalina" },
+                    { 33, "Perez", 43340922, "Racing", "Martina" },
+                    { 34, "Alvarez", 34640923, "Racing", "Marta" },
+                    { 35, "Perez", 43540924, "Racing", "Natalia" },
+                    { 36, "Garcia", 43640925, "Racing", "Guadalupe" },
+                    { 37, "Fole", 43740926, "Racing", "Oriana" },
+                    { 41, "Suarez", 44140920, "River", "Carla" },
+                    { 39, "Naupari", 43940928, "Racing", "Patricia" },
+                    { 27, "Ramirez", 42740926, "Banfield", "Abril" },
+                    { 42, "Gutierrez", 44240920, "River", "Martina" },
+                    { 43, "Souza", 44340920, "River", "Marta" },
+                    { 44, "Perez", 44440921, "River", "Natalia" },
+                    { 45, "Alvarez", 44540922, "River", "Rocio" },
+                    { 46, "Fole", 44640923, "River", "Micaela" },
+                    { 47, "Gonzalez", 44740924, "River", "Antonela" },
+                    { 48, "Ramirez", 44840925, "River", "Diana" },
+                    { 49, "Jacinto", 44940927, "River", "Gabriela" },
+                    { 38, "Gonzalez", 43840927, "Racing", "Mariana" },
+                    { 26, "Gonzalez", 42640965, "Banfield", "Antonela" },
+                    { 22, "Espinonza", 42240921, "Banfield", "Marta" },
+                    { 23, "Tarantini", 42340922, "Banfield", "Julie" },
+                    { 1, "Suarez", 40640921, "Arsenal", "Carla" },
+                    { 2, "Gutierrez", 41640822, "Arsenal", "Martina" },
+                    { 3, "Souza", 42680923, "Arsenal", "Marta" },
+                    { 4, "Perez", 43640924, "Arsenal", "Natalia" },
+                    { 5, "Alvarez", 44640925, "Arsenal", "Rocio" },
+                    { 6, "Fole", 45640926, "Arsenal", "Micaela" },
+                    { 7, "Gonzalez", 46640927, "Arsenal", "Antonela" },
+                    { 8, "Ramirez", 47640928, "Arsenal", "Beatriz" },
+                    { 9, "Jacinto", 48640929, "Arsenal", "Gabriela" },
+                    { 10, "Suarez", 49640928, "Arsenal", "Tamara" },
+                    { 25, "Fole", 42540924, "Banfield", "Micaela" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "FutbolDBJugadoras",
+                columns: new[] { "id", "apellido", "dni", "equipojugando", "nombre" },
+                values: new object[,]
+                {
+                    { 11, "Martinez", 41040929, "Arsenal", "Belen" },
+                    { 13, "Alvarez", 94120923, "Independiente", "Martina" },
+                    { 14, "Souza", 41340924, "Independiente", "Eugenia" },
+                    { 15, "Gonzalez", 41440925, "Independiente", "Oriana" },
+                    { 16, "Alvarez", 41540926, "Independiente", "Marina" },
+                    { 17, "Angel", 41640977, "Independiente", "Marta" },
+                    { 18, "Maritini", 41740928, "Independiente", "Romina" },
+                    { 19, "Capelletti", 41940929, "Independiente", "Juliana" },
+                    { 20, "Suarez", 42040928, "Banfield", "Martina" },
+                    { 21, "Gutierrez", 42140929, "Banfield", "Cecilia" },
+                    { 50, "Suarez", 45040956, "River", "Tamara" },
+                    { 12, "Perez", 41140922, "Independiente", "Marina" },
+                    { 51, "Martinez", 45140929, "River", "Belen" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FutbolDBEquipos_equipo",
+                table: "FutbolDBEquipos",
+                column: "equipo",
+                unique: true,
+                filter: "[equipo] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FutbolDBJugadoras_dni",
+                table: "FutbolDBJugadoras",
+                column: "dni",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FutbolDBEquipos");
+
+            migrationBuilder.DropTable(
+                name: "FutbolDBJugadoras");
+        }
+    }
+}
--- Service/futbolDBContext.cs
+++ Service/futbolDBContext.cs
@@ -28,10 +28,7 @@ namespace ApiFutbolTorneo.Service
             var equipo4 = new FutbolDBEquipos() { id = 4, equipo = "Tigre", color = "rojo y azul" };
             var equipo5 = new FutbolDBEquipos() { id = 5, equipo = "Racing", color = "celeste y blanco" };
             var equipo6 = new FutbolDBEquipos() { id = 6, equipo = "River", color = "rojo y blanco" };
-            var equipo7 = new FutbolDBEquipos() { id = 7, equipo = "Boca", color = "azul y amarrillo" };
-            var equipo8 = new FutbolDBEquipos() { id = 8, equipo = "Porvenir", color = "blanco y negro" };
-            var equipo9 = new FutbolDBEquipos() { id = 9, equipo = "Lanus", color = "Granate" };
-            var equipo10 = new FutbolDBEquipos() { id = 10, equipo = "Ferro", color = "verde" };
+
 
             modelBuilder.Entity<FutbolDBEquipos>().HasData(new FutbolDBEquipos[] { 
                 equipo1,
@@ -39,13 +36,130 @@ namespace ApiFutbolTorneo.Service
                 equipo3,
                 equipo4,
                 equipo5,
-                equipo6,
-                equipo7,
-                equipo8,
-                equipo9,
-                equipo10,
+                equipo6
 
             });
+
+
+            //AGREGO DATOS INICIALES A LA TABLA DE JUGADORAS
+            //JUGADORAS DE ARSENAL
+            var jugadora1 = new FutbolDBJugadoras() { id = 1, dni = 40640921, nombre = "Carla", apellido = "Suarez", equipojugando = "Arsenal" };
+            var jugadora2 = new FutbolDBJugadoras() { id = 2, dni = 41640822, nombre = "Martina", apellido = "Gutierrez", equipojugando = "Arsenal" };
+            var jugadora3 = new FutbolDBJugadoras() { id = 3, dni = 42680923, nombre = "Marta", apellido = "Souza", equipojugando = "Arsenal" };
+            var jugadora4 = new FutbolDBJugadoras() { id = 4, dni = 43640924, nombre = "Natalia", apellido = "Perez", equipojugando = "Arsenal" };
+            var jugadora5 = new FutbolDBJugadoras() { id = 5, dni = 44640925, nombre = "Rocio", apellido = "Alvarez", equipojugando = "Arsenal" };
+            var jugadora6 = new FutbolDBJugadoras() { id = 6, dni = 45640926, nombre = "Micaela", apellido = "Fole", equipojugando = "Arsenal" };
+            var jugadora7 = new FutbolDBJugadoras() { id = 7, dni = 46640927, nombre = "Antonela", apellido = "Gonzalez", equipojugando = "Arsenal" };
+            var jugadora8 = new FutbolDBJugadoras() { id = 8, dni = 47640928, nombre = "Beatriz", apellido = "Ramirez", equipojugando = "Arsenal" };
+            var jugadora9 = new FutbolDBJugadoras() { id = 9, dni = 48640929, nombre = "Gabriela", apellido = "Jacinto", equipojugando = "Arsenal" };
+            var jugadora10 = new FutbolDBJugadoras() { id = 10, dni = 49640928, nombre = "Tamara", apellido = "Suarez", equipojugando = "Arsenal" };
+            var jugadora11 = new FutbolDBJugadoras() { id = 11, dni = 41040929, nombre = "Belen", apellido = "Martinez", equipojugando = "Arsenal" };
+            
+
+            //JUGADORAS DE INDEPENDIENTE
+            var jugadora12 = new FutbolDBJugadoras() { id = 12, dni = 41140922, nombre = "Marina", apellido = "Perez", equipojugando = "Independiente" };
+            var jugadora13 = new FutbolDBJugadoras() { id = 13, dni = 94120923, nombre = "Martina", apellido = "Alvarez", equipojugando = "Independiente" };
+            var jugadora14 = new FutbolDBJugadoras() { id = 14, dni = 41340924, nombre = "Eugenia", apellido = "Souza", equipojugando = "Independiente" };
+            var jugadora15 = new FutbolDBJugadoras() { id = 15, dni = 41440925, nombre = "Oriana", apellido = "Gonzalez", equipojugando = "Independiente" };
+            var jugadora16 = new FutbolDBJugadoras() { id = 16, dni = 41540926, nombre = "Marina", apellido = "Alvarez", equipojugando = "Independiente" };
+            var jugadora17 = new FutbolDBJugadoras() { id = 17, dni = 41640977, nombre = "Marta", apellido = "Angel", equipojugando = "Independiente" };
+            var jugadora18 = new FutbolDBJugadoras() { id = 18, dni = 41740928, nombre = "Romina", apellido = "Maritini", equipojugando = "Independiente" };
+            var jugadora19 = new FutbolDBJugadoras() { id = 19, dni = 41940929, nombre = "Juliana", apellido = "Capelletti", equipojugando = "Independiente" };
+
+            //JUGADORAS DE BANFIELD
+            var jugadora20 = new FutbolDBJugadoras() { id = 20, dni = 42040928, nombre = "Martina", apellido = "Suarez", equipojugando = "Banfield" };
+            var jugadora21 = new FutbolDBJugadoras() { id = 21, dni = 42140929, nombre = "Cecilia", apellido = "Gutierrez", equipojugando = "Banfield" };
+            var jugadora22 = new FutbolDBJugadoras() { id = 22, dni = 42240921, nombre = "Marta", apellido = "Espinonza", equipojugando = "Banfield" };
+            var jugadora23 = new FutbolDBJugadoras() { id = 23, dni = 42340922, nombre = "Julie", apellido = "Tarantini", equipojugando = "Banfield" };
+            var jugadora24 = new FutbolDBJugadoras() { id = 24, dni = 42440923, nombre = "Rocio", apellido = "Alvarez", equipojugando = "Banfield" };
+            var jugadora25 = new FutbolDBJugadoras() { id = 25, dni = 42540924, nombre = "Micaela", apellido = "Fole", equipojugando = "Banfield" };
+            var jugadora26 = new FutbolDBJugadoras() { id = 26, dni = 42640965, nombre = "Antonela", apellido = "Gonzalez", equipojugando = "Banfield" };
+            var jugadora27 = new FutbolDBJugadoras() { id = 27, dni = 42740926, nombre = "Abril", apellido = "Ramirez", equipojugando = "Banfield" };
+            var jugadora28 = new FutbolDBJugadoras() { id = 28, dni = 42840927, nombre = "Gabriela", apellido = "Jacinto", equipojugando = "Banfield" };
+            var jugadora29 = new FutbolDBJugadoras() { id = 29, dni = 42940928, nombre = "Tamara", apellido = "Suarez", equipojugando = "Banfield" };
+            var jugadora30 = new FutbolDBJugadoras() { id = 30, dni = 43040929, nombre = "Belen", apellido = "Martinez", equipojugando = "Banfield" };
+            var jugadora31 = new FutbolDBJugadoras() { id = 31, dni = 43140900, nombre = "Violeta", apellido = "Espinoza", equipojugando = "Banfield" };
+
+           
+            //JUGADORAS DE RACING
+            var jugadora32 = new FutbolDBJugadoras() { id = 32, dni = 43240921, nombre = "Catalina", apellido = "Godoy", equipojugando = "Racing" };
+            var jugadora33 = new FutbolDBJugadoras() { id = 33, dni = 43340922, nombre = "Martina", apellido = "Perez", equipojugando = "Racing" };
+            var jugadora34 = new FutbolDBJugadoras() { id = 34, dni = 34640923, nombre = "Marta", apellido = "Alvarez", equipojugando = "Racing" };
+            var jugadora35 = new FutbolDBJugadoras() { id = 35, dni = 43540924, nombre = "Natalia", apellido = "Perez", equipojugando = "Racing" };
+            var jugadora36 = new FutbolDBJugadoras() { id = 36, dni = 43640925, nombre = "Guadalupe", apellido = "Garcia", equipojugando = "Racing" };
+            var jugadora37 = new FutbolDBJugadoras() { id = 37, dni = 43740926, nombre = "Oriana", apellido = "Fole", equipojugando = "Racing" };
+            var jugadora38 = new FutbolDBJugadoras() { id = 38, dni = 43840927, nombre = "Mariana", apellido = "Gonzalez", equipojugando = "Racing" };
+            var jugadora39 = new FutbolDBJugadoras() { id = 39, dni = 43940928, nombre = "Patricia", apellido = "Naupari", equipojugando = "Racing" };
+            var jugadora40 = new FutbolDBJugadoras() { id = 40, dni = 44040929, nombre = "Nicole", apellido = "Jacinto", equipojugando = "Racing" };
+            
+
+            //JUGADORAS DE RIVER
+            var jugadora41 = new FutbolDBJugadoras() { id = 41, dni = 44140920, nombre = "Carla", apellido = "Suarez", equipojugando = "River" };
+            var jugadora42 = new FutbolDBJugadoras() { id = 42, dni = 44240920, nombre = "Martina", apellido = "Gutierrez", equipojugando = "River" };
+            var jugadora43 = new FutbolDBJugadoras() { id = 43, dni = 44340920, nombre = "Marta", apellido = "Souza", equipojugando = "River" };
+            var jugadora44 = new FutbolDBJugadoras() { id = 44, dni = 44440921, nombre = "Natalia", apellido = "Perez", equipojugando = "River" };
+            var jugadora45 = new FutbolDBJugadoras() { id = 45, dni = 44540922, nombre = "Rocio", apellido = "Alvarez", equipojugando = "River" };
+            var jugadora46 = new FutbolDBJugadoras() { id = 46, dni = 44640923, nombre = "Micaela", apellido = "Fole", equipojugando = "River" };
+            var jugadora47 = new FutbolDBJugadoras() { id = 47, dni = 44740924, nombre = "Antonela", apellido = "Gonzalez", equipojugando = "River" };
+            var jugadora48 = new FutbolDBJugadoras() { id = 48, dni = 44840925, nombre = "Diana", apellido = "Ramirez", equipojugando = "River" };
+            var jugadora49 = new FutbolDBJugadoras() { id = 49, dni = 44940927, nombre = "Gabriela", apellido = "Jacinto", equipojugando = "River" };
+            var jugadora50 = new FutbolDBJugadoras() { id = 50, dni = 45040956, nombre = "Tamara", apellido = "Suarez", equipojugando = "River" };
+            var jugadora51 = new FutbolDBJugadoras() { id = 51, dni = 45140929, nombre = "Belen", apellido = "Martinez", equipojugando = "River" };
+
+
+            modelBuilder.Entity<FutbolDBJugadoras>().HasData(new FutbolDBJugadoras[] {
+                jugadora1,
+                jugadora2,
+                jugadora3,
+                jugadora4,
+                jugadora5,
+                jugadora6,
+                jugadora7,
+                jugadora8,
+                jugadora9,
+                jugadora10,
+                jugadora11,
+                jugadora12,
+                jugadora13,
+                jugadora14,
+                jugadora15,
+                jugadora16,
+                jugadora17,
+                jugadora18,
+                jugadora19,
+                jugadora20,
+                jugadora21,
+                jugadora22,
+                jugadora23,
+                jugadora25,
+                jugadora26,
+                jugadora27,
+                jugadora28,
+                jugadora29,
+                jugadora30,
+                jugadora31,
+                jugadora32,
+                jugadora33,
+                jugadora34,
+                jugadora35,
+                jugadora36,
+                jugadora37,
+                jugadora38,
+                jugadora39,
+                jugadora41,
+                jugadora42,
+                jugadora43,
+                jugadora44,
+                jugadora45,
+                jugadora46,
+                jugadora47,
+                jugadora48,
+                jugadora49,
+                jugadora50,
+                jugadora51
+
+            });
+
 
             base.OnModelCreating(modelBuilder);
 
