ddc2bfbc460e28e92ca0f41eb92ff4b36ad5aba9
facundoN1997 <facundonicolasangel@gmail.com>
2021-08-08 23:08:02 -0300

    En esta versión todavía no se soluciono el error de crear una BD, inicializada con una serie de datos por defecto.

--- Service/futbolDBContext.cs
+++ Service/futbolDBContext.cs
@@ -14,11 +14,47 @@ namespace ApiFutbolTorneo.Service
     {
         public FutbolDBContext(DbContextOptions<FutbolDBContext> options) : base(options)
         {
-           
+            options.SetInitializer<FutbolDBContext>(new UniDBInitializer<FutbolDBContext>());
         }
 
         public DbSet<FutbolDBEquipos> FutbolDBEquipos { get; set; }
         public DbSet<FutbolDBJugadoras> FutbolDBJugadoras { get; set; }
+
+
+        private class UniDBInitializer<T> : DropCreateDatabaseAlways<FutbolDBContext>
+        {
+
+            protected override void Seed(FutbolDBContext context)
+            {
+
+                IList<Student> students = new List<Student>();
+
+                students.Add(new Student()
+                {
+                    FirstMidName = "Andrew",
+                    LastName = "Peters",
+                    EnrollmentDate = DateTime.Parse(DateTime.Today.ToString())
+                });
+
+                students.Add(new Student()
+                {
+                    FirstMidName = "Brice",
+                    LastName = "Lambson",
+                    EnrollmentDate = DateTime.Parse(DateTime.Today.ToString())
+                });
+
+                students.Add(new Student()
+                {
+                    FirstMidName = "Rowan",
+                    LastName = "Miller",
+                    EnrollmentDate = DateTime.Parse(DateTime.Today.ToString())
+                });
+
+                foreach (Student student in students)
+                    context.Students.Add(student);
+                base.Seed(context);
+            }
+        }
 
     }
 }
